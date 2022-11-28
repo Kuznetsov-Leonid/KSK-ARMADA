718564c212ee9c95c3bae60a2327c24449f40767
Kuznetsov Leonid <orlan1211@yahoo.com>
2022-11-28 10:56:13 +0300

    ShortAboutUs

---
+++ armada_app/components/TitleComponent.jsx
@@ -0,0 +1,10 @@
+const TitleComponent = ({children}) => {
+    return(
+        <>
+        <div className="TitleComponent">
+            <h1>{children}</h1>
+        </div>
+        </>
+    );
+}
+export default TitleComponent;
---
+++ armada_app/components/regular/shortAboutUs.jsx
@@ -0,0 +1,95 @@
+import Styles from '../../styles/ShortAboutUs.module.scss';
+import TitleComponent from '../TitleComponent';
+import { Container, CardGroup, Card, Row, Col } from 'react-bootstrap';
+import Image from 'next/image';
+import { BG_NONE } from '../styleProps';
+import ImgShortAboutUs from '../../public/imgComponents/imgShortAboutUs.png';
+//-------------------------------------------------------
+import Check from '../../public/ui_Kit/check.svg';
+import Hanger from '../../public/ui_Kit/hanger.svg';
+import Home from '../../public/ui_Kit/Home.svg';
+import Login from '../../public/ui_Kit/Login.svg';
+import Profile from '../../public/ui_Kit/Profile.svg';
+//-------------------------------------------------------
+
+const ShortAboutUs = () => {
+    const ShortAboutUsAPI = [
+        {id:1, img: Login, txt:'Опытный квалифицированный персонал находится в конюшне круглосуточно.'},
+        {id:2, img: Home, txt:'Конюшня с высокими потолками и большими окнами. Денники стандартные, оборудованные автопоилками.'},
+        {id:3, img: Hanger, txt:'Для владельцев: комната отдыха с бытовой техникой, индивидуальные шкафчики, амуничники.'},
+        {id:4, img: Check, txt:'С нами сотрудничают лучшие ковали и конеперевозчики!'},
+        {id:5, img: Profile, txt:'До конюшни легко добраться как на автомобиле, так и общественным транспортом.'},
+    ];
+    const BenefitsItem = (props) => {
+        const ITEM_IMG = props.ITEM_IMG;
+        const ITEM_TXT = props.ITEM_TXT;
+
+        return(
+            <>
+                <div className={Styles.ShortAboutUs_Benefits}>
+                    <Col> 
+                        <div className={Styles.ShortAboutUs_Benefits_item}>
+                            <div className={Styles.ShortAboutUs_Benefits_item_img}>
+                                <Image src={ITEM_IMG.img}/>
+                            </div>
+                            <p>{ITEM_TXT.txt}</p>
+                        </div>
+                    </Col>
+                </div>
+            </>
+        );
+    }
+
+    const BenefitsList = (props) => {
+        const DATA = props.DATA;
+        const ITEMS = DATA.map((item) => {
+            return <BenefitsItem
+                key      = {item.id}
+                ITEM_IMG = {item}
+                ITEM_TXT = {item}
+            />
+        });
+        return (
+            <>
+                <Row lg={2} style={{'justify-content': 'center'}}>{ITEMS}</Row>
+            </>
+        );
+    }
+
+
+    return(
+        <>
+            <div className={Styles.ShortAboutUs}>
+                <Container>
+                    <TitleComponent>Коротко о нас</TitleComponent>
+                    <Card style={BG_NONE} className={Styles.ShortAboutUs_on}>
+                            <div className={Styles.ShortAboutUs_img}>
+                                <Image 
+                                    style = {{'width':'100%', 'height':'auto'}} 
+                                    src   = {ImgShortAboutUs} 
+                                    alt   = 'Фотография: лошадь привязанная за недауздок, стоит в помещении конюшни.'
+                                />
+                            </div>
+                        </Card>
+                    <CardGroup>
+                        <Card style={BG_NONE} className={Styles.ShortAboutUs_off}>
+                            <div className={Styles.ShortAboutUs_img}>
+                                <Image 
+                                    style = {{'width':'100%', 'height':'auto'}} 
+                                    src   = {ImgShortAboutUs} 
+                                    alt   = 'Фотография: лошадь привязанная за недауздок, стоит в помещении конюшни.'
+                                />
+                            </div>
+                        </Card>
+                        <Card style={BG_NONE}>
+                            <h4>Создавать комфорт для вас и лошадей наша работа </h4>
+                            <BenefitsList DATA = {ShortAboutUsAPI}/>
+                        </Card>
+                    </CardGroup>
+                </Container>
+            </div>
+        </>
+    );
+}
+
+export default ShortAboutUs;
